35b17e6d5abfdbbfe4d7a0d8a4e77f3bd8820c7d
mahmoudamanaa <54070697+mahmoudamanaa@users.noreply.github.com>
2023-07-24 20:50:26 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,8 +40,8 @@ export default async function Home({
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+              {allCars?.map((car, i) => (
+                <CarCard key={i} car={car} />
               ))}
             </div>
             <ShowMore
